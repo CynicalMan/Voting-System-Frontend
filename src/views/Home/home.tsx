3bcdb41bf4d4a34d219cdd9f04a5c59fa0c10885
Omar <95493823+omarkhaleddd@users.noreply.github.com>
2024-05-27 19:20:16 +0300

    initialized the project using vite , added routing needded , added the skeletions of the components

---
+++ src/views/Home/home.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class home extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>home</div>
+    )
+  }
+}
